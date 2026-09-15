4eaa71ebc6c95d7f358a94d1e5b89141f83f431a
somechazzy <chazzireno@gmail.com>
2026-09-15 14:39:25 +0200

    Set defaults for Discord webhook username and avatar

---
+++ scripts/sql/incremental_schema/0.12.0.sql
@@ -0,0 +1,12 @@
+UPDATE `settings`
+SET `data` = 'Saberr'
+WHERE `code` = 'DISCORD_WEBHOOK_USERNAME'
+  AND `data` IS NULL;
+UPDATE `settings`
+SET `data` = 'https://raw.githubusercontent.com/Saberr-app/Saberr/refs/heads/master/assets/legacy/logo.jpg'
+WHERE `code` = 'DISCORD_WEBHOOK_AVATAR_URL'
+  AND `data` IS NULL;
+
+UPDATE `saberr_metadata`
+SET `data` = JSON_SET(`data`, '$.version', '0.12.0')
+WHERE `code` = 'db_schema_metadata';
